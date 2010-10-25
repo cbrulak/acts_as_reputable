4f8b55a4c8c46fc7a3cff7e490b9efc30d403ae2
Chris Brulak <ccmbrulak@yahoo.ca>
2010-10-24 23:05:34 -0600

    correct typo

--- generators/acts_as_reputable/acts_as_reputable_generator.rb
+++ generators/acts_as_reputable/acts_as_reputable_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators/active_record'
 class ActsAsReputableGenerator < ActiveRecord::Generators::Base
   argument :name, :default => "migration"
   
-  def generate_pingback_migration
+  def generate_reputation_migration
     migration_template "migrations/create_reputations.rb", "db/migrate/create_reputations.rb"
   end
   
